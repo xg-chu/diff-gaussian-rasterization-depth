ad46541ae8e73a91c918fb6b872fc1dc52048f6f
Xuangeng Chu <xg.chu@outlook.com>
2024-09-27 23:17:58 +0900

    update

--- setup.py
+++ setup.py
@@ -15,11 +15,11 @@ import os
 os.path.dirname(os.path.abspath(__file__))
 
 setup(
-    name="diff_gaussian_rasterization",
-    packages=['diff_gaussian_rasterization'],
+    name="splatting_with_depth_32d",
+    packages=['splatting_with_depth_32d'],
     ext_modules=[
         CUDAExtension(
-            name="diff_gaussian_rasterization._C",
+            name="splatting_with_depth_32d._C",
             sources=[
             "cuda_rasterizer/rasterizer_impl.cu",
             "cuda_rasterizer/forward.cu",
